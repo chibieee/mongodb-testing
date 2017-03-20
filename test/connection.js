f8c0bff432827fb6e64cba8972568313b2d75c0d
chibieee <gregneo21@gmail.com>
2017-03-19 23:10:09 -0400

    changed promises to ES6, fixed saving data test.

--- test/connection.js
+++ test/connection.js
@@ -1,11 +1,25 @@
 const mongoose = require('mongoose');
 
-// Connect to mongoDB
+//fixed deprecated warning, using ES6 Promises
+mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost/test');
 
-mongoose.connection.once('open', function() {
-    console.log('Connection has been made.');
-}).on('error', function(error) {
-    console.log('Connection error:', error);
+before((done) => {
+    // Connect to mongoDB
+
+    mongoose.connect('mongodb://localhost/test');
+
+    mongoose.connection.once('open', () => {
+        console.log('Connection has been made.');
+        done();
+    }).on('error', (error) => {
+        console.log('Connection error:', error);
+    });
+});
+
+//Drop the character collection before each test
+beforeEach((done) => {
+    mongoose.connection.collections.mariochars.drop(() => {
+        done();
+    });
 });
